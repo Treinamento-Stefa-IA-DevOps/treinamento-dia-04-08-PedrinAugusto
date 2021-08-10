f2d8e2d304e27a454718d1c91d3605414d55ab17
Pedro Augusto <pedrin.22augusto@gmail.com>
2021-08-09 22:10:15 -0300

    Atualização do recebimento dos dados

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -3,11 +3,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 @app.post('/model')
+
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+
+def titanic(Sex: int, Age: float, Lifeboat: int, Pclass: int):
+    
+    with open('model/Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
+    
+    data = [Sex, Age, Lifeboat, Pclass]
+    result = titanic.predict(data)
+
+
 
 @app.get('/model')
+    
 def get():
-    return {'hello':'test'}
+    return {"survived": "bool", "status": "int", "message": "string"}
